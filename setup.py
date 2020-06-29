f20e4303a5adeec1f5fda05d40d9f18f66acbdf2
Siudya <32456796+Siudya@users.noreply.github.com>
2020-06-29 19:08:10 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ import os
 import shutil
 import sys
 import subprocess
-
+from distutils.dir_util import copy_tree
+hw_data_files = []
 if os.geteuid() != 0:
     print("This program must be run as root. Aborting.")
     sys.exit(1)
@@ -11,7 +12,23 @@ if os.geteuid() != 0:
 if os.environ['BOARD'] != 'Pynq-Z2' and os.environ['BOARD'] != 'Pynq-Z1':
     print("Only supported on a Pynq-Z2 and Pynq-Z1 Board")
     exit(1)
+	
+def copy_notebooks():
+    src_nb_dir = os.path.join(f'', 'pynq_notebook')
+    dst_nb_dir = os.path.join(os.environ['PYNQ_JUPYTER_NOTEBOOKS'], 'ORB_FPGA')
+    if os.path.exists(dst_nb_dir):
+        shutil.rmtree(dst_nb_dir)
+    copy_tree(src_nb_dir, dst_nb_dir)
 
+def copy_overlays():
+    src_ol_dir = os.path.join('hw/', '')
+    dst_ol_dir = os.path.join(f'/usr/local/lib/python3.6/dist-packages/ORB_FPGA/overlays', '')
+    copy_tree(src_ol_dir, dst_ol_dir)
+    hw_data_files.extend([os.path.join("..", dst_ol_dir, f) for f in os.listdir(dst_ol_dir)])
+	
+	
+copy_notebooks()
+copy_overlays()
 setup(
 	name = "ORB_FPGA",
 	version = 1.0,
@@ -20,17 +37,12 @@ setup(
 	author = "Liang Sen",
 
 	include_package_data = True,
-	packages = ['ORB_FPGA'],
+	packages = find_packages(),
 	package_data = {
-	'' : ['*.bit','*.py','*.bin','*.txt', '*.cpp', '*.h', '*.sh','*.hwh'],
+	'' : hw_data_files,
 	},
 	description = "ORB Feature Extractor on PYNQ",
     install_requires=[
-        'pynq','numpy','opencv-python','matplotlib'
+        'pynq','numpy','matplotlib'
     ],
 )
-
-if 'install' in sys.argv:
-	if os.path.isdir(os.environ["PYNQ_JUPYTER_NOTEBOOKS"]+"/ORB_FPGA/"):
-		shutil.rmtree(os.environ["PYNQ_JUPYTER_NOTEBOOKS"]+"/ORB_FPGA/")
-	shutil.copytree("pynq_notebook/",os.environ["PYNQ_JUPYTER_NOTEBOOKS"]+"/ORB_FPGA/")
